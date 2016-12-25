c895a47d8da816a22bab97045b5b6818474c6f83
slankka <cor_twi@outlook.com>
2016-12-25 14:57:23 +0800

    Add binarys file with test.php

---
+++ trace-0.3.0/x86/test.php
@@ -0,0 +1,27 @@
+<?php
+
+class CCommand{
+    public static function func1($paramStr){
+        usleep(3000000);
+        echo json_encode($paramStr).__FUNCTION__.PHP_EOL;
+    }
+
+    public static function func2($arrayObj){
+        if(isset($arrayObj['key1'])){
+            return true;
+        }
+        if(isset($arrayObj[0]['key2'])){
+            return json_encode($arrayObj[0]['key2']);
+        }
+    }
+    public static function main(){
+        static::func1('start');
+        $arrayObj = [["key2"=>["val"=>12]],];
+        CCommand::func3($arrayObj);
+    }
+}
+
+echo "TEST:\n";
+usleep(1000000);
+CCommand::main();
+
